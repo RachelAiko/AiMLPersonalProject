5f45934a144c97ce268c607f5ed83653d9c793b7
Rachel A Matthews <54412503+RachelAiko@users.noreply.github.com>
2020-10-04 22:17:49 -0400

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,75 @@
+/* Oracle AiML course : Node object class that contains Tree Traversal Recursive Methods */
+
+public class Node {
+
+    int data;       // properties of Node object
+    Node left;
+    Node right;
+
+    public Node(int data) {     // constructor that takes data as an integer
+
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    // methods to manipulate node attributes (getters and setters)
+   public void setLeft(Node node) {    // adds a node to the current left node. If current left node is null, set left equal to node.
+        if (left == null)
+            left = node;
+    }
+
+    public void setRight(Node node) {   // adds a node to the current right node. If current right node is null, set right equal to node.
+        if (right == null)
+            right = node;
+    }
+
+    public Node getLeft() {     // returns the current left node
+        return left;
+    }
+
+    public Node getRight() {    // returns the current right node
+        return right;
+    }
+
+    public int getData() {     // returns the data value of current node
+        return data;
+    }
+
+    public void setData(int data) {     // set the data value of current node
+        this.data = data;
+    }
+
+
+    // recursive tree traversal methods
+    void printPreOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+    }
+
+    void printPostOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        printPostOrder(node.left);
+        printPostOrder(node.right);
+    }
+
+    void printInOrder(Node node) {
+
+        if (node == null)
+            return;
+
+        printInOrder(node.left);
+        System.out.print(node.data + " ");
+        printInOrder(node.right);
+    }
+
+}
---
+++ NodeDriver.java
@@ -0,0 +1,28 @@
+/* Driver Class for Node */
+
+public class NodeDriver {
+
+    public static void main(String[] args) {
+
+        Node root  = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+        Node node7 = new Node(7);
+
+        root.setLeft(node2);
+        node2.setLeft(node4);
+        node2.setRight(node5);
+        node5.setLeft(node7);
+        root.setRight(node3);
+        node3.setRight(node6);
+
+        root.printInOrder(root);        // calls method to print In Order traversal
+        System.out.println();
+        root.printPostOrder(root);      // calls method to print Post Order traversal
+        System.out.println();
+        root.printPreOrder(root);       // calls method to print Pre Order traversal
+    }
+}
