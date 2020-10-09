3ceb8cf972457ba2a9a7c88fe8ea83220944dd0e
Rachel A Matthews <54412503+RachelAiko@users.noreply.github.com>
2020-10-09 19:46:05 -0400

    Update NodeDriver.java

--- NodeDriver.java
+++ NodeDriver.java
@@ -3,7 +3,8 @@
 public class NodeDriver {
 
     public static void main(String[] args) {
-
+        
+        // creates the nodes of the binary tree
         Node root  = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
@@ -11,7 +12,8 @@ public class NodeDriver {
         Node node5 = new Node(5);
         Node node6 = new Node(6);
         Node node7 = new Node(7);
-
+        
+        // creates the binary tree itself
         root.setLeft(node2);
         node2.setLeft(node4);
         node2.setRight(node5);
